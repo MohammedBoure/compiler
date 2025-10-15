2d7fe1a21d2fb572e52674be5a6713ee16320800
Ahmm <email>
2025-10-15 21:29:45 +0100

    add prototypes for phase 1,2,and 5
    added files,empty functions and structs for:
    phase1:exo1,2
    phase2:exo3,4
    phase5:exo11,12

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include "utils.c"
+
+//exo11
+int getNextToken(FILE* file){
+}
+
+//exo12
+int main(){
+}
+
---
+++ src/utils.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+// Helper function for functions that arent implemented yet
+int todo(){
+    printf("Not implimented yet, exiting..");
+    exit(74);
+}
+
+
+
+//exo2
+struct Token {
+};
+
+//exo3
+int isKeyword(const char word){
+    todo();
+}
+
+//exo4
+int isIdentifierStart(char c){
+    todo();
+}
+
+//exo4
+int isIdentifierChar(char c){
+    todo();
+}
+
+//exo5
+int isOperatorChat(char c){
+    todo();
+}
+
+
